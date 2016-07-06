652907c104ead92ce4b89a6482c5366b62561a57
Christopher Russell <promethiusinchains@gmail.com>
2016-07-05 20:30:10 -0500

    Finished Simple TCP Client

--- Networking/Networking/TCP_Client/TCP_Client.py
+++ Networking/Networking/TCP_Client/TCP_Client.py
@@ -4,3 +4,17 @@ import socket
 #define targets
 target_host = "www.google.com"
 target_port = 80
+
+#create Socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#connect to target
+client.connect((target_host,target_port))
+
+#send some data
+client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+
+#receive some data
+response = client.recv(4096)
+
+print response
