c42ea006b539f9226c5de5e1461725315ffff2aa
Christopher Russell <promethiusinchains@gmail.com>
2016-07-05 22:15:14 -0500

    Further documented code and fixed some formatting issues.

--- Networking/Networking/DIY_Netcat/DIY_Netcat.py
+++ Networking/Networking/DIY_Netcat/DIY_Netcat.py
@@ -13,6 +13,7 @@ execute = ""
 target = ""
 upload_destination = ""
 port = 0
+
 #define usage text
 def usage():
     print "DIY_Netcat"
@@ -51,22 +52,30 @@ def main():
     except getopt.GetoptError as err:
         print str(err)
         usage()
-
+    #loop through opts defining option as o and corresponding argument as a
     for o,a in opts:
+        #if help is passed print usage
         if o in ("-h", "--help"):
             usage()
+        #if listen is passed set listen to true
         elif o in ("-l","--listen"):
             listen = True
+        #if execute is passed then set execute to the value of the argument passed with it
         elif o in ("-e","--execute"):
             execute = a
+        #if commandshell is passed then set command to true 
         elif o in ("-c","--commandshell"):
             command = True
+        #if upload is passed set upload to value of the argument passed with it
         elif o in ("-u","--upload"):
             upload_destination = a
+        #if target is passed then set target to the value of argument passed with it
         elif o in ("-t","--target"):
             target = a
+        #if port is passed then set port to value of argument passed with it cast to int
         elif o in ("-p","--port"):
             port = int(a)
+        #if none of the above trigger write an a debug exception for the unhandled options
         else:
             assert False, "Unhandled Option"
     #check if we are listening or just sending data from stdin
