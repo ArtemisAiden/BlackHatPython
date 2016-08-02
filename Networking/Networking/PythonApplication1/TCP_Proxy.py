69ef36d44defd746c6d900e6423facf3240a3787
Disc0rdantMel0dy <promethiusinchains@gmail.com>
2016-08-01 18:42:36 -0600

    Completed TCP_Proxy.py with all supporting functions.

--- Networking/Networking/PythonApplication1/TCP_Proxy.py
+++ Networking/Networking/PythonApplication1/TCP_Proxy.py
@@ -2,6 +2,7 @@ import sys
 import socket
 import threading
 
+#This function sets up our local listener 
 def server_loop(local_host, local_port, remote_host, remote_port, receive_first):
 	#create raw IPv4 socket
 	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -25,6 +26,97 @@ def server_loop(local_host, local_port, remote_host, remote_port, receive_first)
 		#start a thread for the connection
 		proxy_thread = threading.Thread(target=proxy_handler,args=(client_socket,remote_host,remote_port,receive_first))
 		proxy_thread.start
+
+#This function handles the proxying of data between local and remote hosts
+def proxy_handler(client_socket, remote_host,remote_port,receive_first):
+	#first create raw socket for remote connection
+	remote_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	#connect to the remote host
+	remote_socket.connect((remote_host,remote_port))
+
+	#receive data from remote host if specified
+	if receive_first == True:
+		remote_buffer = receive_from(remote_socket)
+		hexdump(remote_buffer)
+		#send data to response handler
+		remote_buffer = response_handler(remote_buffer)
+		#check to see if there is data to send to our local client and if so send it
+		if len(remote_buffer):
+			print "[<==] Sending %d bytes to local host." % len(remote_buffer)
+			client_socket.send(remote_buffer)
+	#define loop to read from local
+		#send to remote, send to local
+	#lather rinse repeat
+	while True:
+		#read from local host
+		local_buffer = receive_from(client_socket)
+		if len(local_buffer):
+			print "[==>] Received %d bytes from local host." % len(local_buffer)
+			hexdump(local_buffer)
+			#send local buffer to request handler
+			local_buffer = request_handler(local_buffer)
+			#send data to remote host
+			remote_socket.send(local_buffer)
+			print "[==>] Sent to remote host"
+			#receive response back from remote host
+			remote_buffer = receive_from(remote_socket)
+			if len(remote_buffer):
+				print "[<==] Received %d bytes from remote host." % len(remote_buffer)
+				hexdump(remote_buffer)
+				#send response to response handler
+				remote_buffer = response_handler(remote_buffer)
+				#send response to local socket
+				client_socket.send(remote_buffer)
+				print "[<==] Sent to local host."
+			if not len(local_buffer) or len(remote_buffer):
+				client_socket.close()
+				remote_socket.close()
+				print "No more data. Closing connections."
+
+				break
+
+#This function takes bytes and converts to hex
+def hexdump(src, length=16):
+	#create empty list to store result in
+	result = []
+	#nifty way to define variable based on result of if statements
+	#set digits to 4 if src is unicode and 2 otherwise
+	digits  = 4 if isinstance(src, unicode) else 2
+	for i in xrange(0, len(src), length):
+		s = src[i:i+length]
+		hexa = b' '.join(["%0*X" % (digits, ord(x)) for x in s])
+		text = b' '.join([x if 0x20 <= ord(x) < 0x7F else b'.' for x in s])
+		result.append(b"%0C %-*s" % (i,length*(digits+1), hexa, text))
+	print b'\n'.join(result)
+
+#This function reads data from remote host and returns the buffer of data
+def receive_from(connection):
+	#initialize buffer to contain string
+	buffer = ""
+	#Set a 2 second timeout (may need to be adjusted based on connection to target)
+	connection.settimeout(2)
+	try:
+		#keep reading into buffer until there is no more data or we timeout
+		while True:
+			data = connection.recv(4096)
+			if not data:
+				break
+			buffer += data
+	except:
+		pass
+	return buffer
+
+#This function modifies any request destined for the remote host
+def request_handler(buffer):
+	#TODO: perform packet modifcations
+	return buffer
+
+#This function modifies any responses destined for the local host
+def response_handler(buffer):
+	#TODO: perform packet modifications
+	return buffer
+
+#This function represent the main execution flow of the program
 def main():
 	#verify that all 5 necessary arguments were provided and if not print usage and exit
 	if len(sys.argv[1:]) != 5:
