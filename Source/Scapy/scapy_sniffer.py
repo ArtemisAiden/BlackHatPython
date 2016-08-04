a332c7acc83a7a8b91c57ded126f2ee1ec6534fb
ArtemisAiden <promethiusinchains@gmail.com>
2016-08-04 11:21:52 -0500

    coded basic scapy sniffer

---
+++ Source/Scapy/scapy_sniffer.py
@@ -0,0 +1,9 @@
+#import scapy 
+from scapy.all import *
+
+#Create packet callback function
+def packet_callback(packet):
+    print packet.show()
+
+#fire up sniffer
+sniff(prn=packet_callback,count 1)
